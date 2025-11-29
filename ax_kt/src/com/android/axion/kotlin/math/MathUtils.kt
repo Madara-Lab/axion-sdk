41952079966a9949ab1b301acf40e9fa2860c070
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-29 08:42:34 +0800

    [axion_sdk] fixing ax_compose library
    Change-Id: I75e6abe1f81aecaac4ec82eba5bdb8cc8571fc23
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- ax_kt/src/com/android/axion/kotlin/math/MathUtils.kt
+++ ax_kt/src/com/android/axion/kotlin/math/MathUtils.kt
@@ -1,13 +1,33 @@
 package com.android.axion.kotlin.math
 
 import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
-inline fun lerp(start: Float, stop: Float, fraction: Float): Float =
-    start + fraction * (stop - start)
+val Number.sldp: Dp
+    @Composable get() = (this.toFloat() * LocalContext.current.scaleRatioLocked).dp
+
+val Number.sdp: Dp
+    @Composable get() = (this.toFloat() * LocalContext.current.scaleRatio).dp
+
+fun lerp(start: Float, stop: Float, fraction: Float): Float {
+    return start + fraction * (stop - start)
+}
+
+val Context.scaleRatioLocked: Float
+    get() {
+        val displayMetrics = resources.displayMetrics
+        val sw = minOf(displayMetrics.widthPixels, displayMetrics.heightPixels) / displayMetrics.density
+        val ratio = sw / 420f
+        return ratio
+    }
 
 val Context.scaleRatio: Float
     get() {
         val displayMetrics = resources.displayMetrics
         val sw = minOf(displayMetrics.widthPixels, displayMetrics.heightPixels) / displayMetrics.density
-        return if (sw > 620f) 1f else sw / 420f
+        val ratio = if (sw > 620f) 1f else sw / 420f
+        return ratio
     }
